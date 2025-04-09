617312f508a23cb1d9f80f41fa22b11b92ecd4f2
ladev <daanisimov04@gmail.com>
2025-04-10 03:37:03 +0500

    gitignore

--- pkg/sendMessage/sendMessage.go
+++ pkg/sendMessage/sendMessage.go
@@ -18,14 +18,14 @@ func SendMessage(cfg *config.Config, logger *zap.Logger, to string, subject stri
 	dialer := gomail.NewDialer("smtp.mail.ru", 587, cfg.SenderEmail, cfg.SenderPassword)
 
 	if err := dialer.DialAndSend(msg); err != nil {
-		logger.Error(CannotSendMessage(to, err))
-		return fmt.Errorf(CannotSendMessage(to, err))
+		logger.Error(cannotSendMessage(to, err))
+		return fmt.Errorf(cannotSendMessage(to, err))
 	}
 
 	logger.Info("Successfully sent message")
 	return nil
 }
 
-func CannotSendMessage(to string, err error) string {
+func cannotSendMessage(to string, err error) string {
 	return fmt.Sprintf("cannot send message to %s : %v", to, err)
 }
