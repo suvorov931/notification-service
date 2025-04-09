aedb0d33d4d884ee6ed49ff7cf86dcddf2215e8a
ladev <daanisimov04@gmail.com>
2025-04-10 03:27:40 +0500

    method sendnotofocation

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"net"
 	"notification/internal/config"
+	"notification/internal/service"
+	"notification/pkg/api"
 	"notification/pkg/logger"
-	"notification/pkg/sendMessage"
 )
 
 func main() {
@@ -20,25 +23,24 @@ func main() {
 		l.Fatal("failed to read config", zap.Error(err))
 	}
 
-	fmt.Println(cfg)
-
-	//lis, err := net.Listen("tcp", "0.0.0.0:50051")
-	//if err != nil {
-	//	l.Fatal("failed to listen", zap.Error(err))
-	//}
-	//
-	//srv := service.New()
-	//server := grpc.NewServer(grpc.UnaryInterceptor(logger.Interceptor(l)))
-	//api.RegisterNotificationServiceServer(server, srv)
-	//
-	////go func() {
-	//l.Info(fmt.Sprintf("listening on port 50051"))
-	//if err := server.Serve(lis); err != nil {
-	//	l.Fatal("failed to serve", zap.Error(err))
-	//}
-	////}()
-
-	if err := sendMessage.SendMessage(cfg, l, "daanisimov04@gmail.com", "hi", "hello"); err != nil {
-		l.Fatal("failed to send message", zap.Error(err))
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		l.Fatal("failed to listen", zap.Error(err))
 	}
+
+	srv := service.New(cfg, l)
+	interceptor := grpc.UnaryInterceptor(logger.Interceptor(l))
+	s := grpc.NewServer(interceptor)
+	api.RegisterNotificationServiceServer(s, srv)
+
+	//go func() {
+	l.Info(fmt.Sprintf("listening on port 50051"))
+	if err := s.Serve(lis); err != nil {
+		l.Fatal("failed to serve", zap.Error(err))
+	}
+	//}()
+
+	//if err := sendMessage.SendMessage(cfg, l, "daanisimov04@gmail.com", "hi", "hello"); err != nil {
+	//	l.Fatal("failed to send message", zap.Error(err))
+	//}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -2,23 +2,33 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/uuid"
+	"go.uber.org/zap"
+	"notification/internal/config"
 	"notification/pkg/api"
+	"notification/pkg/sendMessage"
 )
 
 type Service struct {
 	api.NotificationServiceServer
+	config *config.Config
+	logger *zap.Logger
 }
 
-func New() *Service {
-	return &Service{}
-}
-
-func NewNot() api.Notification {
-
+func New(config *config.Config, logger *zap.Logger) *Service {
+	return &Service{
+		config: config,
+		logger: logger,
+	}
 }
 
 func (s *Service) SendNotification(ctx context.Context, request *api.SendNotificationRequest) (*api.SendNotificationResponse, error) {
+	err := sendMessage.SendMessage(s.config, s.logger, request.Mail, request.Subject, request.Text)
+	if err != nil {
+		return nil, fmt.Errorf("SendNotification: failed to send message: %w", err)
+	}
+
 	id := uuid.New().String()
 
 	return &api.SendNotificationResponse{
