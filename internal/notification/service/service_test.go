972273386d68e68e3616aa08453e888baa2bd11f
ladev <daanisimov04gmail.com>
2025-05-15 03:47:18 +0500

    working tests for service

--- internal/notification/service/service_test.go
+++ internal/notification/service/service_test.go
@@ -3,10 +3,12 @@ package service
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"reflect"
 	"strconv"
 	"testing"
 	"time"
@@ -28,37 +30,73 @@ func TestSendMessage(t *testing.T) {
 	stmpPort, _ := container.MappedPort(ctx, "1025/tcp")
 	port, _ := strconv.Atoi(stmpPort.Port())
 
-	c := config.Config{CredentialsSender: config.CredentialsSender{
+	c := config.CredentialsSender{
 		SenderEmail: "daa@gmail.com",
 		SMTPHost:    "localhost",
 		SMTPPort:    port,
-	}}
-
-	srv := New(&c, zap.NewNop())
-
-	m := Mail{
-		To:      "daanisimov04@gmail.com",
-		Subject: "hi",
-		Message: "hello from go test",
 	}
 
-	srv.SendMessage(ctx, m)
-
-	time.Sleep(time.Second)
+	srv := New(&c, zap.NewNop())
 	httpPort, _ := container.MappedPort(ctx, "8025/tcp")
 	url := fmt.Sprintf("http://localhost:%s/api/v2/messages", httpPort.Port())
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
 
-	r := response{}
-	json.Unmarshal(body, &r)
+	tests := []struct {
+		name    string
+		mail    Mail
+		want    Mail
+		wantErr error
+	}{
+		{
+			name: "successful send",
+			mail: Mail{
+				To:      "daanisimov04@gmail.com",
+				Subject: "hi",
+				Message: "hello from go test",
+			},
+			want: Mail{
+				To:      "daanisimov04@gmail.com",
+				Subject: "hi",
+				Message: "hello from go test",
+			},
+			wantErr: nil,
+		},
+	}
 
-	gotSubject := r.Items[0].Content.Headers["Subject"][0]
-	gotMessage := r.Items[0].Content.Body
-	fmt.Println(gotSubject, m.Subject)
-	fmt.Println(gotMessage, m.Message)
-	fmt.Println(gotSubject == m.Subject, gotMessage == m.Message)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := srv.SendMessage(ctx, tt.mail)
+			time.Sleep(time.Second)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("SendMessage() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			resp, err := http.Get(url)
+			if err != nil {
+				t.Errorf("Cannot get response: %v", err)
+			}
+			defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("Cannot read body: %v", err)
+			}
+
+			got := response{}
+			if err = json.Unmarshal(body, &got); err != nil {
+				t.Errorf("Cannot unmarshal body: %v", err)
+			}
+
+			gotSubject := got.Items[0].Content.Headers["Subject"][0]
+			if !reflect.DeepEqual(gotSubject, tt.want.Subject) {
+				t.Errorf("SendMessage() gotSubject = %v, want %v", gotSubject, tt.want.Subject)
+			}
+
+			gotMessage := got.Items[0].Content.Body
+			if !reflect.DeepEqual(gotMessage, tt.want.Message) {
+				t.Errorf("SendMessage() gotMessage = %v, want %v", gotMessage, tt.want.Message)
+			}
+		})
+	}
 }
 
 type response struct {
